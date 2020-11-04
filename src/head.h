41773ab2ce5aa1915c7e8dceb1f4edde96073565
gaston-duault <61382435+gaston-duault@users.noreply.github.com>
2020-11-04 11:11:42 +0100

    test

--- src/head.h
+++ src/head.h
@@ -1,2 +1,36 @@
 
 
+#define WINDOW_WITDH 791
+#define WINDOW_HEIGHT 575
+
+SDL_Window *window = NULL;
+SDL_Renderer *renderer = NULL;
+
+SDL_Surface *fond = NULL;
+SDL_Texture *texture = NULL;
+SDL_Rect conteneurfond;
+
+void ExitWithError(const char *message)
+{
+    SDL_Log("Erreur : %s > %s\n", message, SDL_GetError());
+    SDL_Quit();
+    exit(EXIT_FAILURE);
+}
+
+void ExitChargement(const char *message, SDL_Renderer *rendu, SDL_Window *window)
+{
+    SDL_Log("Erreur : %s > %s\n", message, SDL_GetError());
+    SDL_DestroyRenderer(rendu);
+    SDL_DestroyWindow(window);
+}
+
+void limit_fps(unsigned int limit)
+{
+    unsigned int ticks = SDL_GetTicks(); //on synchronise le fps
+    if (limit < ticks)
+        return;
+    else if (limit > ticks + 16)
+        SDL_Delay(16);
+    else
+        SDL_Delay(limit - ticks);
+}
